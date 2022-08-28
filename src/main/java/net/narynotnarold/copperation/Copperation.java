4d6a4d5755e2440a925269e14cf710c0c7c91e84
Nary <65220410+nrtcb@users.noreply.github.com>
2022-08-28 22:36:53 +0430

    block addition

--- src/main/java/net/narynotnarold/copperation/Copperation.java
+++ src/main/java/net/narynotnarold/copperation/Copperation.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.narynotnarold.copperation.block.ModBlocks;
 import net.narynotnarold.copperation.item.ModItems;
 import org.slf4j.Logger;
 
@@ -21,6 +22,7 @@ public class Copperation
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
