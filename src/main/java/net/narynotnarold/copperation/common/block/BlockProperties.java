9026c4331ed6c1557f9c9b231602ca5f727df37f
Nary <65220410+nrtcb@users.noreply.github.com>
2022-09-10 11:41:02 +0430

    hey carmen can you fix this pressure plate registry that would be so awesome

--- src/main/java/net/narynotnarold/copperation/common/block/BlockProperties.java
+++ src/main/java/net/narynotnarold/copperation/common/block/BlockProperties.java
@@ -6,15 +6,19 @@ import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 
 public class BlockProperties {
-    //Other building blocks ig
+    //Misc building blocks
     public static final BlockBehaviour.Properties STEEL_BLOCK = BlockBehaviour.Properties.of(Material.HEAVY_METAL, MaterialColor.COLOR_GRAY).strength(6F).sound(SoundType.NETHERITE_BLOCK);
     public static final BlockBehaviour.Properties COPPER_DROPPER = BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE).strength(2F).sound(SoundType.COPPER);
     public static final BlockBehaviour.Properties VENT = BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE).strength(6F).sound(SoundType.COPPER);
-    public static final BlockBehaviour.Properties COPPER_LEVER = BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE).strength(6F).sound(SoundType.COPPER).instabreak().noCollission().dynamicShape();
     //Copper Pipe stuff!!!!!
     public static final BlockBehaviour.Properties COPPER_PIPE = BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE).strength(6F).sound(SoundType.COPPER);
     public static final BlockBehaviour.Properties EXPOSED_COPPER_PIPE = BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE).strength(6F).sound(SoundType.COPPER);
     public static final BlockBehaviour.Properties WEATHERED_COPPER_PIPE = BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_LIGHT_GREEN).strength(6F).sound(SoundType.COPPER);
     public static final BlockBehaviour.Properties OXIDIZED_COPPER_PIPE = BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_GREEN).strength(6F).sound(SoundType.COPPER);
+
+
+    //Redstone Blocks
+    public static final BlockBehaviour.Properties COPPER_LEVER = BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE).strength(6F).sound(SoundType.COPPER).instabreak().noCollission().dynamicShape();
+    public static final BlockBehaviour.Properties COPPER_PRESSURE_PLATE = BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE).strength(6F).sound(SoundType.COPPER).noOcclusion();
 
 }
--- src/main/java/net/narynotnarold/copperation/common/registry/CBlocks.java
+++ src/main/java/net/narynotnarold/copperation/common/registry/CBlocks.java
@@ -4,27 +4,36 @@ import com.teamabnormals.blueprint.core.util.registry.BlockSubRegistryHelper;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DropperBlock;
+import net.minecraft.world.level.block.PressurePlateBlock;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
 import net.narynotnarold.copperation.Copperation;
 import net.narynotnarold.copperation.common.block.BlockProperties;
 
+import static net.narynotnarold.copperation.common.block.BlockProperties.*;
+
 @Mod.EventBusSubscriber(modid = Copperation.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CBlocks {
 
     public static final BlockSubRegistryHelper HELPER = Copperation.REGISTRY_HELPER.getBlockSubHelper();
 
-    //Other Building Blocks
+    //Misc Building Blocks
     public static final RegistryObject<Block> STEEL_BLOCK = HELPER.createBlock("steel_block", () -> new Block(BlockProperties.STEEL_BLOCK), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> COPPER_DROPPER = HELPER.createBlock("copper_dropper", () -> new DropperBlock(BlockProperties.COPPER_DROPPER), CreativeModeTab.TAB_REDSTONE);
     public static final RegistryObject<Block> VENT = HELPER.createBlock("vent", () -> new RotatedPillarBlock(BlockProperties.VENT), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> COPPER_LEVER = HELPER.createBlock("copper_lever", () -> new RotatedPillarBlock(BlockProperties.COPPER_LEVER), CreativeModeTab.TAB_REDSTONE);
 
     //Copper Pipes
     public static final RegistryObject<Block> COPPER_PIPE = HELPER.createBlock("copper_pipe", () -> new RotatedPillarBlock(BlockProperties.COPPER_PIPE), CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> EXPOSED_COPPER_PIPE = HELPER.createBlock("exposed_copper_pipe", () -> new RotatedPillarBlock(BlockProperties.EXPOSED_COPPER_PIPE), CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> WEATHERED_COPPER_PIPE = HELPER.createBlock("weathered_copper_pipe", () -> new RotatedPillarBlock(BlockProperties.WEATHERED_COPPER_PIPE), CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> OXIDIZED_COPPER_PIPE = HELPER.createBlock("oxidized_copper_pipe", () -> new RotatedPillarBlock(BlockProperties.OXIDIZED_COPPER_PIPE), CreativeModeTab.TAB_BUILDING_BLOCKS);
+
+    //Redstone Blocks
+    public static final RegistryObject<Block> COPPER_LEVER = HELPER.createBlock("copper_lever", () -> new RotatedPillarBlock(BlockProperties.COPPER_LEVER), CreativeModeTab.TAB_REDSTONE);
+    public static final RegistryObject<Block> COPPER_PRESSURE_PLATE = HELPER.createBlock("copper_pressure_plate", () -> new PressurePlateBlock(BlockProperties.COPPER_PRESSURE_PLATE), CreativeModeTab.TAB_REDSTONE);
+
+    //Dropper Blocks
+    public static final RegistryObject<Block> COPPER_DROPPER = HELPER.createBlock("copper_dropper", () -> new DropperBlock(BlockProperties.COPPER_DROPPER), CreativeModeTab.TAB_REDSTONE);
+
 
 }
