5f4e2cb5a876771ba43c166df14a9efe616572a4
Carmenboy <76805533+Carmenboy1@users.noreply.github.com>
2022-09-14 09:40:42 +1200

    Updated Copper Pipe to now Rotate 6 ways!

--- src/main/java/net/narynotnarold/copperation/common/block/CopperPipeBlock.java
+++ src/main/java/net/narynotnarold/copperation/common/block/CopperPipeBlock.java
@@ -2,7 +2,7 @@ package net.narynotnarold.copperation.common.block;
 
 import net.minecraft.world.level.block.DirectionalBlock;
 
-public class CopperPipeBlock extends DirectionalBlock {
+public class CopperPipeBlock extends DirectionalBlockGood {
 
     public CopperPipeBlock(Properties p_52591_) {
         super(p_52591_);
